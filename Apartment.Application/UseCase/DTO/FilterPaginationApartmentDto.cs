6b96dcec3a4b2f82c94a828e3831cf3c36b5277a
dzabula <75902523+dzabula@users.noreply.github.com>
2023-06-10 12:24:40 +0200

    Add files via upload

---
+++ Apartment.Application/UseCase/DTO/FilterPaginationApartmentDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apartment.Application.UseCase.DTO
+{
+    public class FilterPaginationApartmentDto 
+    {
+        public string Title { get; set; }
+        public double MinPrice { get; set; }
+        public double MaxPrice { get; set; }   
+        public int CategoryId { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
